a8a85431186965834bebaa1db0b9690f2d85eeb7
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2023-10-20 13:39:01 -0500

    Cambios Richie 2

--- plantas/src/page/album.jsx
+++ plantas/src/page/album.jsx
@@ -23,9 +23,11 @@ function Album() {
 
   return (
     <div className="container">
-      <h1>Plantas</h1>
+      <br></br>
+      <h1>LISTA DE PLANTAS</h1>
+
       {loading ? (
-        <p>Cargando...</p>
+        <p>CARGANDO DATOS...</p>
       ) : (
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           {data.map((plant) => (
@@ -38,32 +40,35 @@ function Album() {
                     alt={plant.common_name}
                   />
                 ) : (
-                  <div className="placeholder-image">Imagen no disponible</div>
+                  <div className="placeholder-image">
+                    NO SE ENCOTRO UNA IMAGEN
+                  </div>
                 )}
-                <div className="card-body">
-                  <h5 className="card-title">{plant.common_name}</h5>
-                  <p className="card-text">
-                    <strong>Nombre Científico:</strong> {plant.scientific_name[0]}
-                    <br />
-                    <strong>Ciclo:</strong> {plant.cycle}
-                    <br />
-                    <strong>Riego:</strong> {plant.watering}
-                    <br />
-                    <strong>Exposición al Sol:</strong> {plant.sunlight[0]}
-                  </p>
-                  <div className="d-flex justify-content-between align-items-center">
+                <div className="card-body d-flex justify-content-center align-items-center">
+                  <div className="text-center">
+                    <h5 className="card-title">{plant.common_name}</h5>
+                    <p className="card-text">
+                      <strong>NOMBRE CIENTIFICO:</strong>{" "}
+                      {plant.scientific_name[0]}
+                      <br />
+                      <strong>CICLO:</strong> {plant.cycle}
+                      <br />
+                      <strong>RIEGO:</strong> {plant.watering}
+                      <br />
+                      <strong>LUZ SOLAR:</strong> {plant.sunlight[0]}
+                    </p>
                     <div className="btn-group">
                       <button
                         type="button"
                         className="btn btn-sm btn-outline-secondary"
                       >
-                        Ver
+                        VER PLANTA
                       </button>
                       <button
                         type="button"
                         className="btn btn-sm btn-outline-secondary"
                       >
-                        Editar
+                        EDITAR PLANTA
                       </button>
                     </div>
                   </div>
